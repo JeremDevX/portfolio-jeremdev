89cb7bad423913348eba4cd6fde47ed1680e68dd
Lavergnat Jérémie <lavergnatj@outlook.fr>
2024-09-17 09:29:14 +0200

    disallow to index cv page

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/private/",
+      disallow: ["/private/", "/cv_jeremdevx.pdf"],
     },
     sitemap: "https://jeremdevx.com/sitemap.xml",
   };
